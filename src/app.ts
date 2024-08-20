0bef5559fde69cbd64f502d76b4bba7e394c1dcb
Yoan Naydenov <ynaydenov@quickbase.com>
2024-08-20 11:33:40 +0300

    Added authentication which checks for a token in the request headers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
-import { ApolloServer} from "apollo-server";
+import { ApolloServer, AuthenticationError } from "apollo-server";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
+import * as dotenv from "dotenv";
 
-const server = new ApolloServer({typeDefs, resolvers});
+dotenv.config();
+
+const authenticationContextMiddleware = ({ req }: { req: any }) => {
+  const authHeaderUser: string = req.headers.authorization || "";
+  const expectedHeader: string = process.env.AUTH_TOKEN!;
+
+  if (authHeaderUser !== expectedHeader) {
+    throw new AuthenticationError("Unauthorized");
+  }
+
+  return {};
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authenticationContextMiddleware,
+});
 
 server.listen().then(({ url }) => {
   console.log(`Server started at ${url}`);
