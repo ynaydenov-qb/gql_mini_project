5bc7e8d99d387839bb24d03aea76267bfeca0209
Yoan Naydenov <ynaydenov@quickbase.com>
2024-08-21 10:25:55 +0300

    Exported the authentication middleware in another folder
    Exported the authentication middleware in another folder named middleware, this would allow for scalability in later stages if more middleware functions are needed.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,8 @@
 import { ApolloServer, AuthenticationError } from "apollo-server";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
-import * as dotenv from "dotenv";
 import { logger } from "./logger";
-
-dotenv.config();
-
-const authenticationContextMiddleware = ({ req }: { req: any }) => {
-  const authHeaderUser: string = req.headers.authorization || "";
-  const expectedHeader: string = process.env.AUTH_TOKEN!;
-
-  if (authHeaderUser !== expectedHeader) {
-    logger.error(`Unauthorized access attempt: ${req.method} ${req.url}`);
-    throw new AuthenticationError("Unauthorized");
-  }
-  logger.info(
-    `Received request: ${req.method} ${req.url} with headers: ${JSON.stringify(
-      req.headers
-    )}`
-  );
-
-  return {};
-};
+import { authenticationContextMiddleware } from "./middleware/authentication";
 
 const server = new ApolloServer({
   typeDefs,
---
+++ src/middleware/authentication.ts
@@ -0,0 +1,22 @@
+import { AuthenticationError } from "apollo-server";
+import { logger } from "../logger";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const authenticationContextMiddleware = ({ req }: { req: any }) => {
+  const authHeaderUser: string = req.headers.authorization || "";
+  const expectedHeader: string = process.env.AUTH_TOKEN!;
+
+  if (authHeaderUser !== expectedHeader) {
+    logger.error(`Unauthorized access attempt: ${req.method} ${req.url}`);
+    throw new AuthenticationError("Unauthorized");
+  }
+  logger.info(
+    `Received request: ${req.method} ${req.url} with headers: ${JSON.stringify(
+      req.headers
+    )}`
+  );
+
+  return {};
+};
