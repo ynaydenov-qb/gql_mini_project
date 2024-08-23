f13768c6bbee89861f5f83250a402a13273a23cb
Yoan Naydenov <ynaydenov@quickbase.com>
2024-08-23 11:43:03 +0300

    Revert "Separate resolvers in different files and combine them finally"
    This reverts commit da0e5daa1df9a9751ef8145cdbc106fe64ca4efd.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+import { ApolloServer, AuthenticationError } from "apollo-server";
+import { Request } from "express";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { logger } from "./logger";
+import { authenticationContextMiddleware } from "./middleware/authentication";
+import { loggingContextMiddleware } from "./middleware/logging";
+
+const createContext = ({ req }: {req : Request}) => {
+  // Initialize an empty context object
+  let context = {};
+
+  // Apply authentication and logging middleware
+  context = { ...context, ...authenticationContextMiddleware({ req }) };
+  context = { ...context, ...loggingContextMiddleware({ req }) };
+
+  return context;
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: createContext,
+});
+
+server.listen().then(({ url }) => {
+  logger.info(`Server started at ${url}`);
+});
